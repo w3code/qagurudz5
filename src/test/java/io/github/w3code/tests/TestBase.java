e31bd8e716e610db45e757258c3ceea55625b786
Alex <qatechnix@gmail.com>
2021-11-29 10:29:17 +0700

    feat: add selenoid support

--- src/test/java/io/github/w3code/tests/TestBase.java
+++ src/test/java/io/github/w3code/tests/TestBase.java
@@ -3,6 +3,7 @@ package io.github.w3code.tests;
 import com.codeborne.selenide.Configuration;
 import io.github.w3code.pages.RegistrationPage;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
@@ -10,5 +11,12 @@ public class TestBase {
     @BeforeAll
     static void startMaximized() {
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoide.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
     }
 }
